5c35f50f2862e669e7c990a83f9b7fe800a72222
zuk <yisakov@kse.org.ua>
2024-02-18 16:12:43 +0200

    WORKING ASL HOOORAY

--- 3rdAss/BST.cs
+++ 3rdAss/BST.cs
@@ -1,5 +1,6 @@
 ﻿namespace _3rdAss;
 
+
 public class BST
 {
     public KeyValuePair pair ;
@@ -8,26 +9,68 @@ public class BST
     public BST greater ;
 
     public void add(KeyValuePair input )
+    {
+        Balance();
+        if (pair == null)
         {
-            if (pair == null)
+            pairValue = CalculateHash(input.Key);
+            pair = input;
+        }
+        else
+        {
+            if (CalculateHash(input.Key) > pairValue)
             {
-                pairValue = CalculateHash(input.Key) ;
-                pair = input ;
+                if (greater == null)
+                {
+                    greater = new BST();
+                }
+
+                greater.add(input);
             }
             else
             {
-                if (CalculateHash(input.Key) > pairValue)
+                if (lesser == null)
                 {
-                    if (greater == null){greater = new BST() ;}
-                    greater.add(input);
+                    lesser = new BST();
                 }
-                else
-                {
-                    if (lesser == null){lesser = new BST() ;}
-                    lesser.add(input);
-                }
+
+                lesser.add(input);
             }
         }
+    }
+
+    public int Balance()
+    {
+        var lefChildHeight = lesser != null ? lesser.Balance() : 0 ;
+        var rightChildHeight = greater != null ? greater.Balance() : 0 ;
+        switch (lefChildHeight - rightChildHeight)
+        {
+            case > 1:
+                var SwapRootToRight = new BST();
+                SwapRootToRight.add(pair);
+                SwapRootToRight.greater = greater ;
+                SwapRootToRight.lesser = lesser.greater ;
+                lesser.greater = SwapRootToRight ;
+                pair = lesser.pair ;
+                pairValue = lesser.pairValue ;
+                greater = lesser.greater ;
+                lesser = lesser.lesser ;
+                return Math.Max( lefChildHeight , rightChildHeight ) + 1 ;
+            case < -1:
+                var SwapToLeft = new BST();
+                SwapToLeft.add(pair) ;
+                SwapToLeft.lesser = lesser ;
+                SwapToLeft.greater = greater.lesser ;
+                greater.lesser = SwapToLeft ;
+                pair = greater.pair ;
+                pairValue = greater.pairValue ;
+                lesser = greater.lesser ;
+                greater = greater.greater ;
+                return Math.Max( lefChildHeight , rightChildHeight ) + 1 ;
+            default :
+                return Math.Max( lefChildHeight , rightChildHeight ) + 1 ;
+        }
+    }
     
     public string GetValueWithKey(string key)
     {
@@ -48,5 +91,4 @@ public class BST
         }
         return result ; 
     }
-
 }
